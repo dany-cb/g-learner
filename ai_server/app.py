07094d120f7c8851848458d48a955a7e39b6e4a8
dany-cb <danjes002@gmail.com>
2023-03-02 15:27:06 +0530

    feat: add py server

---
+++ ai_server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from YTScraper import YTScraper
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Crawler(Resource):
+    def post(self):
+        data = request.get_json()
+        print(data)
+        keywords = data["keywords"]
+        scraper = YTScraper(keywords.split())
+
+        return jsonify(scraper.get_video_data())
+    # def get(self):
+    #     args = request.args
+    #     print(args)
+    #     keywords = args["keywords"]
+    #     scraper = YTScraper(keywords.split())
+
+    #     return jsonify(scraper.get_video_data())
+
+
+api.add_resource(Crawler, "/")
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
