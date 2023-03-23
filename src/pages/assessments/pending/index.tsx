0715edcfaead707e9936f848d5c908c1efd71b30
alwinjebastine <alwinjebastine@gmail.com>
2023-03-24 00:10:51 +0530

    feat: changes in layout in others

--- src/pages/assessments/pending/index.tsx
+++ src/pages/assessments/pending/index.tsx
@@ -8,8 +8,14 @@ import {
   theme,
   Badge,
   Avatar,
+  Input,
 } from "antd";
-import { HomeFilled, BellTwoTone } from "@ant-design/icons";
+import {
+  HomeFilled,
+  BellTwoTone,
+  BellOutlined,
+  CalendarOutlined,
+} from "@ant-design/icons";
 
 const { Panel } = Collapse;
 const { Header, Content, Footer } = Layout;
@@ -20,42 +26,81 @@ const Pending = () => {
   } = theme.useToken();
   return (
     <>
-      <Header
-        style={{ padding: 0, background: colorBgContainer }}
-        className="d-flex flex-row-reverse"
-      >
-        <div>
-          <Badge count={3} className="me-3" style={{ fontSize: "13px" }}>
-            <Avatar
-              size={35}
-              shape="circle"
-              style={{ backgroundColor: "white" }}
-              icon={
-                <BellTwoTone style={{ fontSize: "22px" }} className="mb-3" />
-              }
+      <div className="m-3 ms-5 mt-4">
+        <nav className="">
+          <div className="d-flex justify-content-between">
+            <Breadcrumb
+              style={{ minWidth: 350, backgroundColor: "#ffff" }}
+              className="d-flex rounded-4 ps-4 pt-2"
+            >
+              <Breadcrumb.Item>
+                <Link href="/courses">
+                  <HomeFilled style={{ fontSize: "18px" }} />
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link href="/assessments">Assessments</Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>Pending</Breadcrumb.Item>
+            </Breadcrumb>
+            <Input
+              className="rounded-4 ms-4 border border-0"
+              placeholder="🔍 Search here for latest feed"
+              allowClear
+              style={{
+                width: 780,
+                marginRight: 65,
+                height: 43,
+              }}
             />
-          </Badge>{" "}
-          <Avatar
-            style={{ backgroundColor: "#f56a00", verticalAlign: "middle" }}
-            size={35}
-            className="me-5"
-          >
-            U
-          </Avatar>
-        </div>
-      </Header>
+            <div
+              className="d-flex justify-content-between"
+              style={{ marginRight: 85 }}
+            >
+              <Badge
+                count={3}
+                className="bg-light"
+                style={{ fontSize: "13px" }}
+              >
+                <Avatar
+                  className=""
+                  size={45}
+                  shape="square"
+                  style={{ backgroundColor: "#201c1c", lineHeight: "31px" }}
+                  icon={
+                    <BellOutlined
+                      style={{ fontSize: "20px" }}
+                      className="rounded-4"
+                    />
+                  }
+                />
+              </Badge>{" "}
+              <Avatar
+                style={{ backgroundColor: "#201c1c", lineHeight: "31px" }}
+                size={45}
+                shape="square"
+                className="mx-3 rounded-3"
+                icon={
+                  <CalendarOutlined
+                    style={{ fontSize: "20px" }}
+                    className="rounded-4"
+                  />
+                }
+              />{" "}
+              <Avatar
+                style={{ backgroundColor: "#201c1c", verticalAlign: "middle" }}
+                size={45}
+                shape="square"
+                className="rounded-3"
+              >
+                U
+              </Avatar>
+            </div>
+          </div>
+        </nav>
+      </div>
+
       <Content style={{ margin: "0 16px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }} className="d-flex">
-          <Breadcrumb.Item>
-            <Link href="/courses">
-              <HomeFilled style={{ paddingLeft: 5 }} />
-            </Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Link href="/assessments">Assessments</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>Pending</Breadcrumb.Item>
-        </Breadcrumb>
         <div
           style={{
             padding: 24,
@@ -80,7 +125,10 @@ const Pending = () => {
           </Collapse>
         </div>
       </Content>
-      <Footer style={{ textAlign: "center" }}>
+      <Footer
+        className="m-3 rounded-4 bold"
+        style={{ textAlign: "center", backgroundColor: "#fff" }}
+      >
         G-learner ©2023 Created by Scuderia
       </Footer>
     </>
